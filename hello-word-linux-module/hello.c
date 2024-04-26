5007111b3d993b6101fe30e5f8679600412a569b
jimmylu890303 <jimmylu9460@gmail.com>
2024-04-26 15:41:16 +0800

    Add params for linux module & Update Makefile

--- hello-word-linux-module/hello.c
+++ hello-word-linux-module/hello.c
@@ -3,14 +3,19 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+static char *name = "world";        ///< An example LKM argument -- default value is "world"
+module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
+
+
 static int hello_init(void) {
-    printk(KERN_INFO "Hello, world\n");
+    printk(KERN_INFO "Hello, %s\n", name);
     return 0;
 }
 
 static void hello_exit(void)
 {
-    printk(KERN_INFO "Goodbye, cruel world\n");
+    printk(KERN_INFO "Goodbye, cruel %s\n", name);
 }
 
 module_init(hello_init);
